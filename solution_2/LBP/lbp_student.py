8758fc91248ad42e10e8589538800203ac6d5daf
vincent <vincentcheng@929@gmail.com>
2020-06-26 12:48:10 +0800

    Add solution 2

--- solution_2/LBP/lbp_student.py
+++ solution_2/LBP/lbp_student.py
@@ -9,6 +9,19 @@ def original_lbp(image):
     lbp_image = np.zeros((rows - 2, cols - 2), np.uint8)
 
     # 计算每个像素点的lbp值，具体范围如上lbp_image
+    binary_values = [0]*8
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            binary_values[0] = 0 if (image[i, j] > image[i-1, j-1]) else 1
+            binary_values[1] = 0 if (image[i, j] > image[i-1, j]) else 1
+            binary_values[2] = 0 if (image[i, j] > image[i-1, j+1]) else 1
+            binary_values[3] = 0 if (image[i, j] > image[i, j-1]) else 1
+            binary_values[4] = 0 if (image[i, j] > image[i, j+1]) else 1
+            binary_values[5] = 0 if (image[i, j] > image[i+1, j-1]) else 1
+            binary_values[6] = 0 if (image[i, j] > image[i+1, j]) else 1
+            binary_values[7] = 0 if (image[i, j] > image[i+1, j+1]) else 1
+            # print(int("".join(str(x) for x in binary_values),2))
+            lbp_image[i-1, j-1] = int("".join(str(x) for x in binary_values),2)
 
     return lbp_image
 
