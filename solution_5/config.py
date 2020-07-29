204e93632cbaff9f34b5a632af4d770318709eba
vincent <vincentcheng929@gmail.com>
2020-07-29 13:33:38 +0800

    Update losses

--- solution_5/config.py
+++ solution_5/config.py
@@ -1,13 +1,13 @@
 
 
 # margin type
-margin_type = 'Softmax'
+margin_type = 'ArcFace'
 feature_dim = 512
 
 
 # training config
 BATCH_SIZE = 256
-LR = 1e-4
-EPOCHS = 200
-STEPS = [20, 60, 120]
+LR = 1e-3
+EPOCHS = 20
+STEPS = [4, 10, 18]
 USE_CUDA = True
